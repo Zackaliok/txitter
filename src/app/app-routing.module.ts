ebbfc11222465c0b1875898e871cc2b6a4ddc9ac
Tristan Fumiere <zackaliok.contact@gmail.com>
2022-04-06 21:03:18 +0200

    Pushing existing content + update gitignore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { ExploreComponent } from './explore/explore.component';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { ParametersComponent } from './parameters/parameters.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'explore', component: ExploreComponent},
+  {path: 'notifications', component: NotificationsComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: 'bookmarks', component: BookmarksComponent},
+  {path: 'lists', component: ListsComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'parameters', component: ParametersComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ExploreComponent } from './explore/explore.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { MessagesComponent } from './messages/messages.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { ListsComponent } from './lists/lists.component';
+import { UserComponent } from './user/user.component';
+import { ParametersComponent } from './parameters/parameters.component';
+import { FastExploreComponent } from './fast-explore/fast-explore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    SideBarComponent,
+    ExploreComponent,
+    NotificationsComponent,
+    MessagesComponent,
+    BookmarksComponent,
+    ListsComponent,
+    UserComponent,
+    ParametersComponent,
+    FastExploreComponent
   ],
   imports: [
     BrowserModule,
